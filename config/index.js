a2f7e03e03a90a6e97b93a142176da442d543ae8
jackdizhu <376365334@qq.com>
2018-08-20 17:35:04 +0800

    增加 数据库端口

--- config/index.js
+++ config/index.js
@@ -5,6 +5,7 @@ module.exports = {
     username: 'root',
     password: 'root',
     host: '127.0.0.1',
+    port: '3306',
     dialect: 'mysql',
     pool: {
       max: 5,
--- database/index.js
+++ database/index.js
@@ -3,6 +3,7 @@ var config = require('../config/index')
 
 module.exports = new Sequelize(config.db.database, config.db.username, config.db.password, {
   host: config.db.host, // 数据库地址
+  port: config.db.port || 3306, // 数据库端口
   dialect: config.db.dialect, // 指定连接的数据库类型
   pool: {
     max: config.db.pool.max, // 连接池中最大连接数量
